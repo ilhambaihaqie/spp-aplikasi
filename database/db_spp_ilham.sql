c767eb59a132e287ded61d096ff45aea07636377
ilhambnf <68320410+ilhambaihaqie@users.noreply.github.com>
2021-04-03 10:49:46 +0700

    Add files via upload

--- database/db_spp_ilham.sql
+++ database/db_spp_ilham.sql
@@ -1,1 +1,219 @@
+-- phpMyAdmin SQL Dump
+-- version 4.7.4
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 03, 2021 at 03:58 AM
+-- Server version: 10.1.29-MariaDB
+-- PHP Version: 7.1.12
 
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `db_spp_ilham`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `kelas`
+--
+
+CREATE TABLE `kelas` (
+  `id_kelas` int(11) NOT NULL,
+  `nama_kelas` varchar(10) NOT NULL,
+  `kompetensi_keahlian` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `kelas`
+--
+
+INSERT INTO `kelas` (`id_kelas`, `nama_kelas`, `kompetensi_keahlian`) VALUES
+(1, 'X IPA ', 'IPA'),
+(2, 'XI IPS ', 'IPS'),
+(3, 'XII BAHASA', 'Bahasa Indonesia');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pembayaran`
+--
+
+CREATE TABLE `pembayaran` (
+  `id_pembayaran` int(11) NOT NULL,
+  `id_petugas` int(11) NOT NULL,
+  `nisn` varchar(10) NOT NULL,
+  `tgl_bayar` date NOT NULL,
+  `bulan_dibayar` varchar(13) NOT NULL,
+  `tahun_dibayar` varchar(4) NOT NULL,
+  `id_spp` int(11) NOT NULL,
+  `jumlah_bayar` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `pembayaran`
+--
+
+INSERT INTO `pembayaran` (`id_pembayaran`, `id_petugas`, `nisn`, `tgl_bayar`, `bulan_dibayar`, `tahun_dibayar`, `id_spp`, `jumlah_bayar`) VALUES
+(1, 1, '0034508827', '2021-01-15', 'Januari', '2021', 1, 200000),
+(3, 2, '0034508827', '2021-02-16', 'februari', '2021', 1, 200000),
+(4, 1, '0034508827', '2021-03-09', 'maret', '2021', 1, 200000),
+(5, 1, '0034508827', '2021-04-14', 'april', '2021', 1, 200000),
+(8, 2, '0034508827', '2021-05-19', 'Mei', '2021', 1, 200000),
+(9, 2, '0034508827', '2021-06-11', 'Juni', '2021', 1, 200000),
+(10, 2, '0034508827', '2021-07-19', 'juli', '2021', 1, 200000),
+(11, 2, '0034508827', '2021-08-29', 'agustus', '2021', 1, 200000),
+(13, 2, '0034508827', '2021-09-15', 'september', '2021', 1, 200000),
+(14, 1, '0034508827', '2021-10-20', 'oktober', '2021', 1, 200000),
+(15, 1, '0034508827', '2021-11-18', 'november', '2021', 1, 200000),
+(16, 1, '0034508827', '2021-12-27', 'desember', '2021', 1, 200000);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `petugas`
+--
+
+CREATE TABLE `petugas` (
+  `id_petugas` int(11) NOT NULL,
+  `username` varchar(30) NOT NULL,
+  `password` varchar(32) NOT NULL,
+  `nama_petugas` varchar(35) NOT NULL,
+  `level` enum('Admin','Petugas','siswa','') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `petugas`
+--
+
+INSERT INTO `petugas` (`id_petugas`, `username`, `password`, `nama_petugas`, `level`) VALUES
+(1, 'Asep', '123', 'H.Asep Saepullah Spd', 'Admin'),
+(2, 'Rachmawati', '321', 'Rachmawati Rahayu Spd', 'Petugas'),
+(3, '0034508827', 'ilham', 'Ilham Baihaqie Nur Falah', 'siswa');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `siswa`
+--
+
+CREATE TABLE `siswa` (
+  `nisn` char(10) NOT NULL,
+  `nis` char(10) NOT NULL,
+  `nama` varchar(35) NOT NULL,
+  `id_kelas` int(11) NOT NULL,
+  `alamat` text NOT NULL,
+  `no_telp` varchar(13) NOT NULL,
+  `id_spp` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `siswa`
+--
+
+INSERT INTO `siswa` (`nisn`, `nis`, `nama`, `id_kelas`, `alamat`, `no_telp`, `id_spp`) VALUES
+('0022126766', '181910545', 'Putri Melida Agrezta', 1, 'Cihanjuang', '08534261233', 1),
+('0029482937', '181910550', 'Salma Nur Aisy', 2, 'Cimahi', '08552351637', 2),
+('0029692434', '181910543', 'Ihsan Nurmansyah', 1, 'Padalarang', '08123142345', 1),
+('0032002614', '181910548', 'Muhammad Yusuf', 2, 'Dago', '081253625136', 2),
+('0034117682', '181910544', 'Lucky Wiratama Suganda', 2, 'Sumur Bandung', '0856242315', 2),
+('0034508827', '181910543', 'Ilham Baihaqie Nur Falah', 3, 'Batujajar Barat', '08654325441', 1),
+('0039758919', '181910551', 'Indah Muslimah', 3, 'Kota Cimahi', '083275686543', 3);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `spp`
+--
+
+CREATE TABLE `spp` (
+  `id_spp` int(11) NOT NULL,
+  `tahun` int(11) NOT NULL,
+  `nominal` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `spp`
+--
+
+INSERT INTO `spp` (`id_spp`, `tahun`, `nominal`) VALUES
+(1, 2021, 200000),
+(2, 2020, 150000),
+(3, 2019, 120000);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `kelas`
+--
+ALTER TABLE `kelas`
+  ADD PRIMARY KEY (`id_kelas`);
+
+--
+-- Indexes for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  ADD PRIMARY KEY (`id_pembayaran`);
+
+--
+-- Indexes for table `petugas`
+--
+ALTER TABLE `petugas`
+  ADD PRIMARY KEY (`id_petugas`);
+
+--
+-- Indexes for table `siswa`
+--
+ALTER TABLE `siswa`
+  ADD PRIMARY KEY (`nisn`);
+
+--
+-- Indexes for table `spp`
+--
+ALTER TABLE `spp`
+  ADD PRIMARY KEY (`id_spp`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `kelas`
+--
+ALTER TABLE `kelas`
+  MODIFY `id_kelas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `pembayaran`
+--
+ALTER TABLE `pembayaran`
+  MODIFY `id_pembayaran` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=17;
+
+--
+-- AUTO_INCREMENT for table `petugas`
+--
+ALTER TABLE `petugas`
+  MODIFY `id_petugas` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- AUTO_INCREMENT for table `spp`
+--
+ALTER TABLE `spp`
+  MODIFY `id_spp` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
